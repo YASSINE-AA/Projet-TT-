4cfe977785cd5dd153460512a1124da55da880fc
Yassine Ahmed Ali <65458854+YASSINE-AA@users.noreply.github.com>
2024-10-24 10:44:45 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
 import Gestion from "./Pages/Gestion";
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        {/* Redirect root path to /login */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/gestion-des-employes" element={<Gestion />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
